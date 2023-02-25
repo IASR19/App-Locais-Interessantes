b359a0649a204a5d41e5b80499d566f35c5fcbc0
Itamar Ribeiro <itamar.asr19@gmail.com>
2023-02-25 04:29:45 -0300

    Criando botão para cidades aleatórias

---
+++ android/app/src/main/kotlin/com/bai/aplicativo/locais/locais_interessantes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bai.aplicativo.locais.locais_interessantes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Locais Interessantes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'App Locais Interessantes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  List locais = [
+    "Belo Horizonte",
+    "São Paulo",
+    "Curitiba",
+    "Rio de Janeiro",
+    "Recife",
+    "Sorocaba",
+    "Santa RIta do Sapucaí",
+    "Pouso Alegre"
+  ];
+  String randomTexto = "";
+
+  void _atualizarTela() {
+    setState(() {
+      randomTexto = locais[new Random().nextInt(locais.length)];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue, // background
+                onPrimary: Colors.white, // foreground
+              ),
+              onPressed: _atualizarTela,
+
+              child: Text('Clique'),
+            ),
+            Text(
+              '$randomTexto',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
